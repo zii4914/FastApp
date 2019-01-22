d86af21d8af958b458aabd49705f0a7e8bf9e9a7
zii <zii4914@163.com>
2019-01-22 16:55:07 +0800

    Dialog

--- app/src/main/java/com/zii/fastapp/MainActivity.java
+++ app/src/main/java/com/zii/fastapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.zii.fastapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.zii.base.util.ToastUtils;
+import com.zii.base.widget.dialog.PopupDialog;
+import com.zii.base.widget.dialog.PopupItemsDialog;
+import com.zii.base.widget.dialog.TranslucenceDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void onTestSome(View view) {
+    PopupItemsDialog dialog = new PopupItemsDialog(this, 180, 0)
+        .addItem("哈哈", new TranslucenceDialog.OnClickChild() {
+          @Override
+          public void onClick(TranslucenceDialog dialog, View view) {
+            dialog.dismiss();
+            ToastUtils.showShort(((TextView) view).getText());
+          }
+        })
+        .addItem("Gou", new TranslucenceDialog.OnClickChild() {
+          @Override
+          public void onClick(TranslucenceDialog dialog, View view) {
+            dialog.dismiss();
+            ToastUtils.showShort(((TextView) view).getText());
+          }
+        });
+    dialog.setLocation(view, "bottom", PopupDialog.ALIGN_LEFT, 0, 0);
+    dialog.show();
   }
 }
